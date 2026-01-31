7cb008ff6dffde384cbd58924403a492dec1fdd0
SimonEkstrom <58439054+SimonEkstrom@users.noreply.github.com>
2026-01-31 17:28:26 +0100

    zoom

--- scripts.js
+++ scripts.js
@@ -3,24 +3,17 @@ const mq = window.matchMedia('(min-width: 576px)');
 function handleScreenChange(e) {
   if (e.matches) {
     // screen is ≥ 576px
-    doDesktopThing();
   } else {
     // screen is < 576px
     document.addEventListener('click', () => {
         if (document.querySelector('.swiper-slide-zoomed')) {
             console.log('Zoomed');
             document.querySelectorAll('.swiper').forEach(el => {
-                el.classList.add('h-100');});
-            document.querySelectorAll('.modal-header').forEach(el => {
-                el.classList.add('position-absolute');
-                el.classList.add('bottom-100');});
+                el.classList.add('swiper-fullscreen');});
         } else {
             console.log('Not Zoomed');
             document.querySelectorAll('.swiper').forEach(el => {
-                el.classList.remove('h-100');});
-            document.querySelectorAll('.modal-header').forEach(el => {
-                el.classList.remove('position-absolute');
-                el.classList.remove('bottom-100');});
+                el.classList.remove('swiper-fullscreen');});
         }
     });
   }
@@ -31,3 +24,17 @@ handleScreenChange(mq);
 
 // listen for changes
 mq.addEventListener('change', handleScreenChange);
+
+swiper.on('zoomChange', (swiper, scale) => {
+    if (e.matches) {
+        // screen is ≥ 576px
+    } else {
+        // screen is < 576px
+        const zoomed = scale > 1;
+
+        swiper.allowTouchMove = !zoomed;
+        swiper.mousewheel && (swiper.mousewheel.enabled = !zoomed);
+        swiper.keyboard && (swiper.keyboard.enabled = !zoomed);
+    }
+});
+
