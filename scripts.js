80dcdb17af3c31612258e7c02e1153e3ed78fafd
SimonEkstrom <58439054+SimonEkstrom@users.noreply.github.com>
2026-01-31 16:50:29 +0100

    zoom

--- scripts.js
+++ scripts.js
@@ -1,19 +1,33 @@
+const mq = window.matchMedia('(min-width: 576px)');
 
+function handleScreenChange(e) {
+  if (e.matches) {
+    // screen is ≥ 576px
+    doDesktopThing();
+  } else {
+    // screen is < 576px
+    document.addEventListener('click', () => {
+        if (document.querySelector('.swiper-slide-zoomed')) {
+            console.log('Zoomed');
+            document.querySelectorAll('.swiper').forEach(el => {
+                el.classList.add('h-100');});
+            document.querySelectorAll('.modal-header').forEach(el => {
+                el.classList.add('position-absolute');
+                el.classList.add('bottom-100');});
+        } else {
+            console.log('Not Zoomed');
+            document.querySelectorAll('.swiper').forEach(el => {
+                el.classList.remove('h-100');});
+            document.querySelectorAll('.modal-header').forEach(el => {
+                el.classList.remove('position-absolute');
+                el.classList.remove('bottom-100');});
+        }
+    });
+  }
+}
 
-document.addEventListener('click', () => {
-    if (document.querySelector('.swiper-slide-zoomed')) {
-        console.log('Zoomed');
-        document.querySelectorAll('.swiper').forEach(el => {
-            el.classList.add('h-100');});
-        document.querySelectorAll('.modal-header').forEach(el => {
-            el.classList.add('position-absolute');
-            el.classList.add('bottom-100');});
-    } else {
-        console.log('Not Zoomed');
-        document.querySelectorAll('.swiper').forEach(el => {
-            el.classList.remove('h-100');});
-        document.querySelectorAll('.modal-header').forEach(el => {
-            el.classList.remove('position-absolute');
-            el.classList.remove('bottom-100');});
-    }
-});
+// run once on load
+handleScreenChange(mq);
+
+// listen for changes
+mq.addEventListener('change', handleScreenChange);
